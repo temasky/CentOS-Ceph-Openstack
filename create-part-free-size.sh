29ecc1eebe515e1a9f2a9d27af46c38ca76a9603
Artem Shvidky <temasky@gmail.com>
2019-08-14 16:41:22 +0300

    Create create-part-free-size.sh

---
+++ create-part-free-size.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+devname=$1
+
+
+start=$(( $(parted $devname 'unit % print' | grep -v extended | tail -2 | grep . | awk '{print $3}' | tr -d %| cut -f1 -d".") + 1 ))
+
+parted $devname mkpart ext2 "$start"% 95%
